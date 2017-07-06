66bd29dd0bbbd6eb10f61d19d8a0d381dba07762
Unknown <francima93@gmail.com>
2017-07-06 13:23:27 +0200

    New code to read and interpret the new dataset.
     Maybe this can be done also in python, since the learning functions are already written in the language.

---
+++ C++_side/DatasetOperations/DatasetUtils.h
@@ -0,0 +1,13 @@
+#ifndef DATASETUTILS_H_INCLUDED
+#define DATASETUTILS_H_INCLUDED
+
+#define nSamples 200
+#define nChannels 8
+
+typedef struct
+{
+
+
+}DataSetStructure;
+
+#endif // DATASETUTILS_H_INCLUDED
